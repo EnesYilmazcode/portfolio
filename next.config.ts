4c6a39d7b138e798da75ee84df6ad42fce71971e
Enes Yilmaz <enesyilmaz5157@gmail.com>
2025-03-22 04:36:23 -0400

    checking

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },
+  basePath: "/enesyilmazcode.github.io", // Sets the correct base path for GitHub Pages
+  assetPrefix: "/enesyilmazcode.github.io",
   devIndicators: false,
   errorIndicators: false,
-  /* config options here */
 };
 
 export default nextConfig;
